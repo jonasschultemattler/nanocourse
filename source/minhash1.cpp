a5fdcfe221fa4121c44cd896e8a973de81170ca7
Schulte-Mattler <adm_js4718fu@mimir.imp.fu-berlin.de>
2025-05-22 23:36:03 +0200

    final

--- source/minhash1.cpp
+++ source/minhash1.cpp
@@ -14,17 +14,6 @@ struct my_traits:seqan3::sequence_file_input_default_traits_dna {
 };
 
 
-std::vector<uint64_t> multi_hash(uint64_t input, size_t N) {
-    std::vector<uint64_t> hashes;
-    for (size_t i = 0; i < N; ++i) {
-        // Combine input with index as salt
-        uint64_t combined = input ^ (0x9e3779b97f4a7c15ULL * i);
-        hashes.push_back(util::Fingerprint(combined));
-    }
-    return hashes;
-}
-
-
 void minhash_sketch(const std::filesystem::path &filepath, uint64_t permutations, uint64_t minhashs[],
                     const uint64_t a[], const uint64_t b[], uint64_t (*hashFunc)(uint64_t))
 {
